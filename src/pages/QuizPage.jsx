339b5fc6e409d88f5e0580c6cc946721190398d3
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-12 14:56:16 +0900

    ButtonコンポーネントにonClickプロパティを追加し、QuizPageで動的にボタンを生成するように変更

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,19 +1,23 @@
 import Button from "../components/Button";
-import Display from "../components/Display"
+import Display from "../components/Display";
 import quizData from "../data/quiz";
 
 export default function QuizPage() {
   const quizIndex = 0;
+  const handleClick = (clickedIndex) => {
+    console.log("clickedIndex:", clickedIndex);
+  };
 
   return (
     <>
-      <Display>
-        {`Q1. ${quizData[quizIndex].question}`}
-      </Display>
-      <Button>{quizData[quizIndex].options[0]}</Button>
-      <Button>{quizData[quizIndex].options[1]}</Button>
-      <Button>{quizData[quizIndex].options[2]}</Button>
-      <Button>{quizData[quizIndex].options[3]}</Button>
+      <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>
+      {quizData[quizIndex].options.map((option, index) => {
+        return (
+          <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+            {option}
+          </Button>
+        );
+      })}
     </>
-  )
+  );
 }
