ced887b781c92a2ebec34a7ac885e02109529696
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-13 01:58:36 +0900

    QuizPageコンポーネントの状態管理を追加し、クイズの回答をログに記録する機能を実装

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,17 +1,45 @@
 import Button from "../components/Button";
 import Display from "../components/Display";
 import quizData from "../data/quiz";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../const";
 
 export default function QuizPage() {
-  const quizIndex = 0;
+  // 第一引数はstate関数、第２引数はset関数
+  const [quizIndex, setQuizIndex] = useState(0);
+  const [answerLogs, setAnswerLogs] = useState([]);
+  const navigate = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length;
+
   const handleClick = (clickedIndex) => {
-    console.log("clickedIndex:", clickedIndex);
+    if(clickedIndex === quizData[quizIndex].answerIndex){
+      setAnswerLogs((prev) => [...prev, true])
+    } else {
+      setAnswerLogs((prev) => [...prev, false])
+    }
+    setQuizIndex((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    if(answerLogs.length === MAX_QUIZ_LEN) {
+      const correctNum = answerLogs.filter((answer) => {
+        return answer === true;
+      })
+      navigate(ROUTES.RESULT, {
+        state: {
+          maxQuizLen: MAX_QUIZ_LEN,
+          correctNum: correctNum
+        }
+      });
+    }
+  }, [answerLogs, MAX_QUIZ_LEN, navigate]);
 
   return (
     <>
-      <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>
-      {quizData[quizIndex].options.map((option, index) => {
+      {/* && で条件付きレンダリング */}
+      {quizData[quizIndex] && <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>}
+      {quizData[quizIndex] && quizData[quizIndex].options.map((option, index) => {
         return (
           <Button key={`option-${index}`} onClick={() => handleClick(index)}>
             {option}
