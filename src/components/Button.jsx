2e154473423ac3eab7e10aaeb44f45c3c85d2bea
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-12 12:36:43 +0900

    Buttonコンポーネントの作成

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import styles from './Button.module.css';
+
+export default function Button({ children }) {
+  return (
+    <button className={styles.button} type="button">
+      <span className={styles.buttonInner}>{ children }</span>
+    </button>
+  )
+}
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,4 +1,6 @@
+import Button from "../components/Button";
 import Display from "../components/Display"
+import quizData from "../data/quiz";
 
 export default function QuizPage() {
   const quizIndex = 0;
@@ -8,7 +10,10 @@ export default function QuizPage() {
       <Display>
         {`Q1. ${quizData[quizIndex].question}`}
       </Display>
-
+      <Button>{quizData[quizIndex].options[0]}</Button>
+      <Button>{quizData[quizIndex].options[1]}</Button>
+      <Button>{quizData[quizIndex].options[2]}</Button>
+      <Button>{quizData[quizIndex].options[3]}</Button>
     </>
   )
 }
