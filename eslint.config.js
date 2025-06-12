ec00be56bd63e3b359e2ddd352cbd081ef4580ad
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-12 10:32:55 +0900

    reactセットアップ

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "no-undef": "error", // 未定義の変数を使用した場合にエラーにする
+      "no-unused-vars": "error", // 宣言したが使われていない変数がある場合にエラーにする
+      'react/prop-types': 'off', // ReactのPropTypesによる型チェックを無効化する（TypeScript使用時などに推奨）
+    },
+  },
+]
